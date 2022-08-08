14126436e871ab4df85d6bc522bde2485df77abf
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-08-08 18:42:25 +0100

    chore: upgrade to MDX 2

--- src/pages/posts/{mdx.fields__slug}.js
+++ src/pages/posts/{mdx.fields__slug}.js
@@ -2,31 +2,32 @@ import React, { Fragment } from 'react';
 import { graphql, Link } from 'gatsby';
 
 import { MDXProvider } from '@mdx-js/react';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import PrismSyntaxHighlight from '../../components/prism-syntax-highlight';
 
 const components = {
   code: ({ children, className }) => {
-    return <PrismSyntaxHighlight className={className}>{children}</PrismSyntaxHighlight>;
+    return className ? (
+      <PrismSyntaxHighlight className={className}>{children}</PrismSyntaxHighlight>
+    ) : (
+      <code>{children}</code>
+    );
   }
 };
 
 const Page = ({
   data: {
     mdx: {
-      frontmatter: { title, date },
-      body
+      frontmatter: { title, date }
     }
-  }
+  },
+  children
 }) => (
   <Fragment>
     <Link to="/">Back</Link>
     <h1>{title}</h1>
     <small>{date}</small>
-    <MDXProvider components={components}>
-      <MDXRenderer>{body}</MDXRenderer>
-    </MDXProvider>
+    <MDXProvider components={components}>{children}</MDXProvider>
   </Fragment>
 );
 
@@ -37,7 +38,6 @@ export const query = graphql`
         title
         date(formatString: "MMMM Do, YYYY")
       }
-      body
     }
   }
 `;
